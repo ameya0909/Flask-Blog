4d97a71b2876cb4deaf0484e43227b31cbb9b4c6
ameya0909 <am.ja.man@gmail.com>
2016-02-10 18:18:07 -0800

    Added login form

--- views.py
+++ views.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from __init__ import app
+from forms import LoginForm
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', title='Home', user="Ameya")
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form, title='Login')
